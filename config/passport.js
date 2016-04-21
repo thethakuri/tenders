c7cf913ef81c3892aa8af1ae86102804a1f4b793
Ashish Singh <thethakuri@gmail.com>
2016-04-21 08:52:27 +0545

    Convert username string to lowercase before performing any operation on them.

--- config/passport.js
+++ config/passport.js
@@ -42,6 +42,8 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
         
+        email = email.toLowerCase();
+ 
         // asynchronous
         // User.findOne wont fire unless data is sent back
         process.nextTick(function() {
@@ -99,6 +101,8 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
+        
+        email = email.toLowerCase();
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
