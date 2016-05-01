6212246dfafdda421801a3a7e2ec33e7ea0f44bb
Ashish Singh <thethakuri@gmail.com>
2016-05-01 10:06:54 +0545

    Second major commit. Added lots of additional features including detail view.

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,5 +1,9 @@
 //Custom jQueries
 $(function (){
+    
+    $('.input-md.date').datepicker({
+        format: 'dd/mm/yyyy'
+    })
     /*
     // Clear button for search field
     $(".clear").css("right", $("#viewSelect > button").width() + 50 +"px");
